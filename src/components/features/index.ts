bfc06c32e3b2e14c0ba64adaa37608e8a1dbdb45
노준석 <joshua052698@gmail.com>
2025-07-23 18:01:22 +0900

    refactor: EvaluationChart 컴포넌트 Pie 차트/불필요 데이터 제거 및 코드 정리

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -1,3 +1,2 @@
-export { default as StatCard } from './StatCard';
-export { default as ConsultantTable } from './ConsultantTable';
-export { default as EvaluationChart } from './EvaluationChart'; 
+export { default as StatCard } from "./StatCard";
+export { default as ConsultantTable } from "./ConsultantTable";
